ba8e1993a3f1c95938e937220606109ecff86bc6
Omar Adrian <oadrian38@gmail.com>
2023-11-28 23:22:37 -0600

    Ya se muestran las publicaciones con sus respectivos datos
    Se visualiza si la publicacion es anonima o no

--- src/server/routes/forum.routes.js
+++ src/server/routes/forum.routes.js
@@ -11,13 +11,20 @@ mongoose.connect(MONGODB_URI)
 
 const forum = Router()
 
-forum.get('/', (req, res) => {
-    if (req.session.user) {
-        const data = req.session.user[0]
-        res.render('forum.ejs', { user: data })
-    } else {
-        console.log('No existe una sesion')
-        res.render('forum.ejs')
+forum.get('/', async (req, res) => {
+
+    try {
+        const posts = await Post.find({})
+        console.log(posts)
+        if (req.session.user) {
+            const data = req.session.user[0]
+            res.render('forum.ejs', { user: data, posts: posts })
+        } else {
+            console.log('No existe una sesion')
+            res.render('forum.ejs', { posts: posts })
+        }
+    } catch (error) {
+        
     }
 })
 
@@ -31,7 +38,7 @@ forum.post('/post', (req, res) => {
             title: title,
             description: description,
             date: new Date(),
-            owner: req.session.user[0].name,
+            owner: req.session.user[0].n_control,
             anonym: anonym === 'on' ? true : false,
             mode: mode,
             comments: []
@@ -39,7 +46,7 @@ forum.post('/post', (req, res) => {
 
         console.log(`Post agregado por ${req.session.user[0].n_control}`)
         post.save();
-        res.redirect('/');
+        res.redirect('/forum');
     }
     // res.send('Se ha publicado un post')
 })
